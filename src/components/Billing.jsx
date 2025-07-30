44dd44a43fc8ea000d3c45b1ac75124bb034c63d
djaberdev <djabert64@gmail.com>
2025-07-30 12:06:37 +0100

    Update favicon and billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -9,7 +9,7 @@ const Billing = () =>  (
                 alt="Bill"
             />
         </div>
-        <div className="flex-1 ${flexStartCol} gap-4">
+        <div className="flex-1 flex flex-col items-start gap-4">
             <h1 className={`${styles.heading}`}>Easily control your billing & invoicing.</h1>
             <p className={`${styles.paragraph}`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
             <div className="flex items-center gap-5">
